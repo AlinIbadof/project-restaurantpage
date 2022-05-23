a029fe87e0819330432061a7f3a8cb3f9ff03a7b
Alin Ibadof <alinibadof@gmail.com>
2022-05-23 20:10:46 +0300

    add rethinked page-load structure

--- src/page-load.js
+++ src/page-load.js
@@ -1,4 +1,26 @@
 function pageLoad() {
+  // Navigation buttons
+  const btnHome = document.createElement("div");
+  btnHome.classList.add("navbtn");
+  btnHome.textContent = "Home";
+
+  const btnMenu = document.createElement("div");
+  btnMenu.classList.add("navbtn");
+  btnMenu.textContent = "Menu";
+
+  const btnContact = document.createElement("div");
+  btnContact.classList.add("navbtn");
+  btnContact.textContent = "Contact";
+
+  const buttons = document.createElement("div");
+  buttons.classList.add("navigation");
+
+  buttons.appendChild(btnHome);
+  buttons.appendChild(btnMenu);
+  buttons.appendChild(btnContact);
+  ////////////////////////////
+
+  // Content
   const title = document.createElement("div");
   title.textContent = "Alin's restaurant";
   title.classList.add("title");
@@ -15,21 +37,36 @@ function pageLoad() {
   taglineDiv.appendChild(taglineP);
 
   const reviewDiv = document.createElement("div");
-  reviewDiv.classList.add("title");
+  reviewDiv.classList.add("review");
   const reviewP = document.createElement("p");
   reviewP.textContent =
     "It's a great experience. The ambiance is very welcoming and charming. Amazing wines, food and service. Staff are extremely knowledgeable and make great recommendations.";
 
   reviewDiv.appendChild(reviewP);
 
+  const contentDiv = document.createElement("div");
+  contentDiv.classList.add("middleContent");
+
+  contentDiv.appendChild(title);
+  contentDiv.appendChild(chefImage);
+  contentDiv.appendChild(taglineDiv);
+  contentDiv.appendChild(reviewDiv);
+  ///////////////////////////////
+
+  // Footer section
+  const footer = document.createElement("div");
+  footer.classList.add("footer");
+  footer.textContent = "@ Alin Ibadof";
+  //////////////////////////////
+
   // creating a new element which will serve as a container for the above strucutre (for easier styling)
   const homePage = document.createElement("div");
   homePage.classList.add("home");
 
-  homePage.appendChild(title);
-  homePage.appendChild(chefImage);
-  homePage.appendChild(taglineDiv);
-  homePage.appendChild(reviewDiv);
+  homePage.appendChild(buttons);
+  homePage.appendChild(contentDiv);
+  homePage.appendChild(footer);
+
   // appending the above created structure to the content div
   const element = document.getElementById("content");
   element.appendChild(homePage);
