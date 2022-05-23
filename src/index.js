d1e626f50cec5a7181f12122fa243fd273c6b150
Alin Ibadof <alinibadof@gmail.com>
2022-05-23 21:16:31 +0300

    fix middle section width

--- src/index.js
+++ src/index.js
@@ -1,10 +1,3 @@
 import pageLoad from "./page-load";
 
 pageLoad();
-
-import pageMenu from "./page-menu";
-
-function clearPage() {
-  const element = document.getElementById("content");
-  element.removeChild(element.firstChild);
-}
